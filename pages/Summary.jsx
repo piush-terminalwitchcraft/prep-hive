bc8634065c373a757716cdf4bebfc60ac338770f
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2023-02-03 09:48:29 +0530

    wewq

--- pages/Summary.jsx
+++ pages/Summary.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import { useRouter } from 'next/router';
 import styles from "../styles/Summary.module.css";
+import React, { useRef, useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+
+var label1; 
+var label2;
+var label3;
+
 
 export default function Summary() {
     const { query } = useRouter();
     console.log(query)
-    if(query.data1 == undefined){
-    }
-    else{
-        console.log(query.data)
-        const label1 = JSON.parse(query.data1), label2 = JSON.parse(query.data2), type = query.type;
-        console.log(label1, label2, type);
-
-    }
+    console.log(query.data)
+    label1 = JSON.parse(query.data1), label2 = JSON.parse(query.data2), type = query.type;
+    console.log(label1, label2, type);
+    const chartData = {
+      labels: Array.from({ length: label1.length }, (_, i) => i),
+      datasets: [
+        {
+          label: 'Data 1',
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: label1,
+        },
+      ],
+    };
 //     const data = [1, 2, 3];
 // const encodedData = encodeURIComponent(JSON.stringify(data));
 // const link = `https://example.com?data=${encodedData}`;
@@ -20,7 +46,7 @@ export default function Summary() {
   return (
     <div className={styles.MainBody}>
         <h1>Summary</h1>
-
+        <Line data={chartData} />
     </div>
   )
 }
